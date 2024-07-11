4152b8a02f480dd77d6d95f7a3a3b71970d75054
Benjamin <benjamin.rogers@merkleinc.com.au>
2024-07-11 16:19:03 +1000

    added Total Current Balance section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,30 @@
 import React from "react";
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotoalBalanceBox";
 
 const Home = () => {
-  return <div>Home</div>;
+  const loggedIn = {
+    firstName: "Ben",
+  };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficently."
+          />
+        </header>
+        <TotalBalanceBox
+          accounts={[]}
+          totalBanks={1}
+          totalCurrentBalance={1250.35}
+        />
+      </div>
+    </section>
+  );
 };
 
 export default Home;
