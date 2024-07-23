6edd6b2ad5d0d4a7662ad8fb0e209ad56c4e75a1
Benjamin Rogers <benjaminrogers@Benjamins-MacBook-Pro.local>
2024-07-24 00:22:18 +1000

    working on sidebar links

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,7 +1,31 @@
+'use client'
+
+import { sidebarLinks } from "@/constants";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
 const SideBar = ({ user }: SiderbarProps) => {
+const pathName = usePathname();
+
   return (
     <section className="sidebar">
-      <nav className="flex flex-col hap-4"></nav>
+      <nav className="flex flex-col gap-4">
+<Link href="/" className="mb-12 cursor-pointer items-center gap-2">
+<Image src='/icons/logo.svg' width={34} height={34} alt="Next-Bank logo" className="size-[24px] max-xl:size-14"/>
+<h1 className="sidebar-logo">Next Bank</h1>
+</Link>
+    {sidebarLinks.map((item) => {
+      const isActive = pathName === item.route || pathName.startsWith(`${item.route}/`);
+        
+      return (
+        <Link href={item.route} key={item.label} className={cn('sidebar-link', {'bg-bank-gradient': isActive })}>
+          {item.label}
+        </Link>
+      )
+    })}
+      </nav>
     </section>
   );
 };
