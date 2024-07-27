b9c28d57339d670772f7d7b3b20befce93572260
Benjamin Rogers <benjaminrogers@Benjamins-MacBook-Pro.local>
2024-07-27 23:48:51 +1000

    created right sidebar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import HeaderBox from "@/components/HeaderBox";
 import TotalBalanceBox from "@/components/TotoalBalanceBox";
+import RightSidebar from "@/components/RightSidebar";
 
 const Home = () => {
   const loggedIn = {
     firstName: "Ben",
+    lastName: 'Rogers',
+    email: 'benjrogers48@gmail.com'
   };
   return (
     <section className="home">
@@ -16,13 +19,14 @@ const Home = () => {
             user={loggedIn?.firstName || "Guest"}
             subtext="Access and manage your account and transactions efficently."
           />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
         </header>
-        <TotalBalanceBox
-          accounts={[]}
-          totalBanks={1}
-          totalCurrentBalance={1250.35}
-        />
       </div>
+      <RightSidebar user={loggedIn} transactions={[]} banks={[{}, {}]} />
     </section>
   );
 };
